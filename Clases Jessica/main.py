5fa9b3fbc6bba386f9647d51cb25466a73e806fa
Jose Mateos <zs24022199@estudiantes.uv.mx>
2026-08-25 18:13:21 -0600

    Actividad Jessica

--- Clases Jessica/main.py
+++ Clases Jessica/main.py
@@ -2,12 +2,12 @@ from abc import ABC, abstractmethod
 
 class Personaje(ABC):
 
-    def_init_(self, nombre, vida):
-    self.nombre = nombre
-    self.vida = vida
+    def __init__(self, nombre, vida):
+        self.nombre = nombre
+        self.vida = vida
 
     def recibir_danio(self, cantidad):
-        self.vida -=cantidad
+        self.vida -= cantidad
         print(f"{self.nombre} recibe {cantidad} de danio")
         print(f"vida restante: {self.vida}")
 
@@ -15,17 +15,16 @@ class Personaje(ABC):
     def atacar(self):
         pass
 
-    class ICurable(ABC):
-
-        @abstractmethods
+class ICurable(ABC):
+        @abstractmethod
         def curar(self, objetivo):
             pass
 
-    class Guerrero(Personaje):
+class Guerrero(Personaje):
         def atacar(self):
             print(f"{self.nombre} ataca con su espada")
 
-    class Mago(Personaje):
+class Mago(Personaje):
         def atacar(self):
             print(f"{self.nombre} ataca con bola de fuego")
 
@@ -33,7 +32,7 @@ class Personaje(ABC):
             obejtivo.vida +=20
             print(f"{self.nombre} cura a {obejtivo.nombre} + 20 puntos de vida")
 
-    class Soporte(Personaje):
+class Soporte(Personaje):
         def atacar(self):
             print(f"{self.nombre} ataca con baston")
 
@@ -41,16 +40,16 @@ class Personaje(ABC):
             obejtivo.vida +=20
             print(f"{self.nombre} cura a {obejtivo.nombre} + 20 puntos de vida")
 
-    guerrero = Guerrero("Guerrero1", 100)
-    mago = Mago("Mago1", 60)
-    soporte = Soporte("Soporte1", 70)
+guerrero = Guerrero("Guerrero1", 100)
+mago = Mago("Mago1", 60)
+soporte = Soporte("Soporte1", 70)
 
-    guerrero.atacar()
-    mago.atacar()
-    soporte.atacar()
+guerrero.atacar()
+mago.atacar()
+soporte.atacar()
 
-    guerrero.recibir_danio(30)
-    mago.curar(guerrero)
-    soporte.curar(guerrero)
+guerrero.recibir_danio(30)
+mago.curar(guerrero)
+soporte.curar(guerrero)
 
-    print(f"vida final de {guerrero.nombre} : {guerrero.vida}")
+print(f"vida final de {guerrero.nombre} : {guerrero.vida}")
